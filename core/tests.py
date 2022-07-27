c85e8e896cc839798be7daca870698bc281123cd
sanjay-grbby <80387693+sanjay-grbby@users.noreply.github.com>
2022-07-27 17:33:28 +0530

    Delete core directory

--- core/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import Client
-from django.test import TestCase
-
-
-class ExampleTestCase(TestCase):
-    def test_index_page(self):
-        c = Client()
-        resp = c.get('/')
-        self.assertEqual(resp.status_code, 200)
--- core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from core import views
-
-urlpatterns = [
-    path('', views.home)
-]
